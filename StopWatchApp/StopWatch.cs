5344534281addec44a8d80f9bd9d1e2c65bb75e7
Majdi Khalil <majdi.khalil@bentley.com>
2017-06-15 10:49:24 -0400

    Stop Watch Console App

---
+++ StopWatchApp/StopWatch.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StopWatchApp
+{
+    public class StopWatch
+    {
+        private DateTime StartTime { get; set; }
+        private DateTime StopTime { get; set; }
+        private bool StopWatchStarted { get; set; }
+        private readonly List<TimeSpan> _durationIntervals;
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                var totalDuration = TimeSpan.Zero;
+                foreach (TimeSpan timeSpan in _durationIntervals)
+                {
+                    totalDuration += timeSpan;
+                }
+                return totalDuration;
+            }
+        }
+
+        public StopWatch()
+        {
+            StopWatchStarted = false;
+            _durationIntervals = new List<TimeSpan>();
+        }
+
+        public void Start()
+        {
+            if (!StopWatchStarted)
+            {
+                StartTime = DateTime.Now;
+                Console.WriteLine("Start Pressed");
+                StopWatchStarted = true;
+
+            }
+            else
+            {
+                throw new InvalidOperationException(nameof(StartTime));
+            }
+
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stop Pressed");
+            if (StopWatchStarted)
+            {
+                StopTime = DateTime.Now;
+                StopWatchStarted = false;
+                AddDurationToList();
+            }
+
+
+        }
+
+        private void AddDurationToList()
+        {
+            _durationIntervals.Add((StopTime - StartTime));
+        }
+
+    }
+}
